b256ecacaf8b0b9c63d746b056333ff9cd7e5823
mike clinton <mikeclinton508@gmail.com>
2026-05-05 01:26:29 +0100

    added the 2 social engineering sweep and revoke

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './index.css'
 import FlashSweep from './Flashsweep'
 import RevokeShield from './RevokeShield'
 
+
 const queryClient = new QueryClient()
 
 // 🛠️ ADDED: Detect the current URL path
